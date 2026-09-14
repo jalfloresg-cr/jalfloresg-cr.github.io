a60f9c47318caa30653e117e6ae4b6bcb50a63b4
Jose <jalfloresg@gmail.com>
2026-09-14 10:26:23 -0600

    feat: Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'José Flores / DevOps Engineer';
+export const SITE_DESCRIPTION = 'DevOps Engineer specializing in Kubernetes, cloud platforms, CI/CD and DevSecOps.';
